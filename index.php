1bd88e5a150393ce6e7d36785015bfbca63bbdd5
Sedelkov Nikolay <Sedelkov_NA@setltech.ru>
2025-09-04 19:57:15 +0300

    welocome php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo rand(0, 1000);
+    ?>
+
+    <h1>
+        <?php
+            $left_bounc = rand(0, 1000);
+            echo $left_bounc;
+            echo '-';
+            echo rand($left_bounc, 2 * $left_bounc);
+        ?>
+    </h1>
+    <a href="/contacts">контакты</a>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero alias voluptates provident quaerat at quisquam eligendi exercitationem animi officiis cumque, totam vero repellat, qui porro iure distinctio tenetur soluta! Assumenda!
+</body>
+</html>
