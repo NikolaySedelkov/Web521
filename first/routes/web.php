0df18a42d9ac242b3ce06a656afab02ce37c011f
Your Name <you@example.com>
2025-10-16 19:58:45 +0300

    group request

--- first/routes/web.php
+++ first/routes/web.php
@@ -19,54 +19,7 @@ Route::name("actor.")->prefix("actor")->group(function() {
      * Path-paramert(variable) - параметры, которые передаются в запросах в виде части пути
      */
     Route::get("/{id}", function(int $id) {
-        $users = [
-            1 => [
-            "name" => "Данила",
-            "surname" => "Козловский",
-            "patronymic" => "Сергеевич",
-            "age" => 40,
-            "email" => "danila.kozlovsky@example.com",
-            "phone" => "+7 900 111-22-33",
-            "roles" => ["user", "editor"],
-            "active" => true,
-            "registered_at" => "2020-05-12",
-            "address" => [
-                "city" => "Москва",
-                "street" => "Тверская",
-                "house" => "1",
-                "zip" => "125009"
-            ],
-            "social" => [
-                "telegram" => "@danila_k",
-                "vk" => "vk.com/danila_k"
-            ]
-        ],
-        2 => [
-            "name" => "Джони",
-            "surname" => "Депп",
-            "patronymic" => null,
-            "age" => 61,
-            "email" => "johnny.depp@example.com",
-            "phone" => "+1 310 555-12-34",
-            "roles" => ["user", "vip"],
-            "active" => false,
-            "registered_at" => "2018-11-03",
-            "address" => [
-                "city" => "Лос‑Анджелес",
-                "street" => "Sunset Blvd",
-                "house" => "100",
-                "zip" => "90028"
-            ],
-            "social" => [
-                "instagram" => "@j_depp_official",
-                "x" => "@j_depp"
-            ]
-        ],
-        ];
-
-        if(array_key_exists($id, $users)) {
-            return view("actor.index", ['user' => $users[$id]]);
-        }
+        
 
         return "Oops";
     })->name("index"); // name=actor.index <-> prefix + name
@@ -78,21 +31,91 @@ Route::name("city.")->prefix("city")->group(function() {
     })->name("list");
 
     Route::get("/{id}", function(int $id) {
-        $cities = [
-            1 => [
-                "name" => "Вологда",
-                "populare" => 300_000
-            ],
-            2 => [
-                "name" => "Кингисепп",
-                "populare" => 20_000
-            ],
-        ];
+        $response = Http::get(route("api.city.index", ["id" => $id]));
 
-        if (array_key_exists($id, $cities)) {
-            return view("city.index", ['city' => $cities[$id]]);
+        if($response->ok()) {
+            return view("city.index", ["user" => $response->json()]);
         }
 
         return "Oops";
     })->name("index");
+});
+
+
+Route::name("api.")->prefix("api")->group(function () {
+    Route::name("actor.")->prefix("actor")->group(function() {
+        Route::get("/{id}", function(int $id) {
+            $users = [
+                    1 => [
+                    "name" => "Данила",
+                    "surname" => "Козловский",
+                    "patronymic" => "Сергеевич",
+                    "age" => 40,
+                    "email" => "danila.kozlovsky@example.com",
+                    "phone" => "+7 900 111-22-33",
+                    "roles" => ["user", "editor"],
+                    "active" => true,
+                    "registered_at" => "2020-05-12",
+                    "address" => [
+                        "city" => "Москва",
+                        "street" => "Тверская",
+                        "house" => "1",
+                        "zip" => "125009"
+                    ],
+                    "social" => [
+                        "telegram" => "@danila_k",
+                        "vk" => "vk.com/danila_k"
+                    ]
+                ],
+                2 => [
+                    "name" => "Джони",
+                    "surname" => "Депп",
+                    "patronymic" => null,
+                    "age" => 61,
+                    "email" => "johnny.depp@example.com",
+                    "phone" => "+1 310 555-12-34",
+                    "roles" => ["user", "vip"],
+                    "active" => false,
+                    "registered_at" => "2018-11-03",
+                    "address" => [
+                        "city" => "Лос‑Анджелес",
+                        "street" => "Sunset Blvd",
+                        "house" => "100",
+                        "zip" => "90028"
+                    ],
+                    "social" => [
+                        "instagram" => "@j_depp_official",
+                        "x" => "@j_depp"
+                    ]
+                ],
+            ];
+
+            if(array_key_exists($id, $users)) {
+                return view("actor.index", ['user' => $users[$id]]);
+            }
+
+            abort(403);
+        });
+    });
+
+    Route::name("city.")->prefix("city")->group(function() {
+        Route::get("/{id}", function(int $id) {
+            $cities = [
+                1 => [
+                    "name" => "Вологда",
+                    "populare" => 300_000
+                ],
+                2 => [
+                    "name" => "Кингисепп",
+                    "populare" => 20_000
+                ],
+            ];
+
+            if(array_key_exists($id, $cities)) {
+                return view("city.index", ['city' => $cities[$id]]);
+            }
+
+            abort(403);
+        })->name("index");
+    });
 });
