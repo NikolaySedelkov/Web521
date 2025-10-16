8ac30f1f917b4f236d77039a86c391dc768bbcc4
Your Name <you@example.com>
2025-10-16 19:49:11 +0300

    group request

--- first/resources/views/actor/list.blade.php
+++ first/resources/views/actor/list.blade.php
@@ -1,6 +1,6 @@
 <h1>Список актеров</h1>
 
 <ul>
-    <li><a href="{{ route("actor", ["id" => 1]) }}">Данила Козловский</a></li>
-    <li><a href="{{ route("actor", ["id" => 2]) }}">Джони Депп</a></li>
+    <li><a href="{{ route("actor.index", ["id" => 1]) }}">Данила Козловский</a></li>
+    <li><a href="{{ route("actor.index", ["id" => 2]) }}">Джони Депп</a></li>
 </ul>
--- first/resources/views/start.blade.php
+++ first/resources/views/start.blade.php
@@ -1,6 +1,6 @@
 <h1>Главная</h1>
 
 <ul>
-    <li><a href="{{ route("list-actors") }}">Актеры</a></li>
-    <li><a href="/city">Города</a></li>
+    <li><a href="{{ route("actor.list") }}">Актеры</a></li>
+    <li><a href="{{ route("city.list") }}">Города</a></li>
 </ul>
