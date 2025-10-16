30c7e0b6953428f85c120cb366f1be90403bc0c5
Your Name <you@example.com>
2025-10-16 19:02:28 +0300

    Имена route

--- first/resources/views/start.blade.php
+++ first/resources/views/start.blade.php
@@ -1,6 +1,6 @@
 <h1>Главная</h1>
 
 <ul>
-    <li><a href="/actor">Актеры</a></li>
+    <li><a href="{{ route("list-actors") }}">Актеры</a></li>
     <li><a href="/city">Города</a></li>
 </ul>
--- first/routes/web.php
+++ first/routes/web.php
@@ -2,8 +2,8 @@
 
 Route::get("/", function() {
     return view("start");
-});
+})->name("home");
 
 Route::get("/actor", function() {
     return view("actor.list");
-});
+})->name("list-actors");
