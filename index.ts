16aec1e8bc0024759ad0cbb13ab1056117215c8c
Your Name <you@example.com>
2025-11-06 20:49:46 +0300

    TS полностью, но по верхам

--- index.ts
+++ index.ts
@@ -65,3 +65,70 @@ const _matrix: string[][] = [
 const combineMatrix: (number | boolean)[] = [];
 
 //  2. Кортеж
+// Массивы, разнотивные, - каждый элемент может иметь какой-то конретный. это массивы конкретной длины!
+const turple: [number, string, undefined] = [1, "2", undefined];
+// useState -> [value, (value) => void]
+
+// Функции
+/**
+ *  (par1: TYPE1, par2: TYPE2, par3: TYPE3, ....) => RETURN_TYPE
+ */
+function plus(a: number, b: number) {
+    return a + b;
+} 
+const handle: (a: number, b: number) => number = plus;
+
+// Объекты
+const _obj: { x: number } = { x: 2 };
+
+// Объявление типа для объекта состоящего из полей up и down, каждое из который имеет тип number
+type Decimal = {
+    up: number,
+    down: number,
+    // children?: Decimal[]
+}
+const _decimal: Decimal = { up: 1, down: 7 };
+
+interface IDecimal {
+    up: number,
+    down: number,
+    // children?: IDecimal[]
+}
+
+interface IRealDecimal extends IDecimal {
+    // znak: boolean;
+    znak: '+' | '-' 
+}
+
+const _realDecimal: IRealDecimal = {
+    up: 12,
+    down: 2,
+    znak: '+'
+}
+
+// Тип, который может иметь любой ключ, но важно чтобы ключом была именно строка, и по значение по этому ключу было число
+type UniversaleObject = {
+    [key: string]: number
+}
+
+const _universaleObj: UniversaleObject = {
+    x: 1,
+    yyy: 12222,
+    'dsfsd_ d': -1,
+    // 5: 1,
+    // 'Hello': 'world!'
+}
+
+// Generics - возможность работы с динамичским типом
+// Параметр функции - тип
+function findMax<T>(
+    numbers: T[], 
+    compare: (a: T, b: T) => boolean
+): T | undefined {
+    if(!numbers.length) return undefined
+
+    return numbers.reduce(
+        (curr, acc) => compare(curr, acc) ? curr : acc,
+        numbers.at(0)!
+    )
+}
